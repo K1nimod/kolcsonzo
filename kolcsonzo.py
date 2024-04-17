651f8eff2c4e339733999046c13bb6427139a346
D1monik <116081418+D1monik@users.noreply.github.com>
2024-04-17 12:14:49 +0200

    coomiiit

--- kolcsonzo.py
+++ kolcsonzo.py
@@ -1,28 +1,45 @@
-class kocsonzo:
-    
-    def __init__(self,nev,jazon,eora,eperc,vora,vperc):
+class Kolcsonzes:
+    def __init__(self,nev,jarmuazonosito,elvitelora,elvitelperc, visszaora, visszaperc):
         self.nev = nev
-        self.jazon = jazon
-        self.eora = eora
-        self.eperc = eperc
-        self.vora = vora
-        self.vperc = vperc
+        self.jarmuazonosito = jarmuazonosito
+        self.elvitelora = elvitelora
+        self.elvitelperc = elvitelperc
+        self.visszaora = visszaora
+        self.visszaperc = visszaperc
+        
+    def __str__(self):
+        return f"{self.nev}{self.jarmuazonosito}{self.elvitelora}{self.elvitelperc}{self.visszaora}{self.visszaperc}"
         
 f = open("kolcsonzesek.txt", "r", encoding="utf-8")
+f.readline()
+lista = []
 
-napisz = -1
 for sor in f:
-    napisz += 1
+    tmp = sor.strip().split(";")
+    lista.append(Kolcsonzes(tmp[0],tmp[1],tmp[2],tmp[3],tmp[4],tmp[5]))
 
-print("5. feladat: Napi kölcsönzések száma: ",napisz )
+f = open("kolcsonzesek.txt", "r", encoding="utf-8")
+f.readline()
 
-"""kolcsonzo = {}
-for sor in f:
-    sor = sor.split(';')
-    if sor[1] in kolcsonzo.keys():
-        kolcsonzo[sor[1]] += 1 
+kolcsonzes = {}
+napi = 0
+db = 0
+
+for line in f:
+    temp = line.strip().split(";")
+    if line[1] in kolcsonzes.keys():
+        kolcsonzes[line[1]] +=1
     else:
-        kolcsonzo[sor[1]] = 1
+        kolcsonzes[line[1]] =1
+    db +=1
+print("5. feladat")
+print(f"{db} kölcsönzés adatai vannak a forrásban")
 
-for k,v in kolcsonzo.items():
-    print(k,v)"""
+
+
+
+
+
+print("10. feladat")
+for k,v in kolcsonzes.items():
+    print(f"{k} - {v}")
